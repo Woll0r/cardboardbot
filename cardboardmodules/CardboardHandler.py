f57d5bdd928def6da5bbcc47367f4d7966cc9a6f
Wolfgang Vandevenne <woll0r@gmail.com>
2014-07-11 23:20:06 +0200

    That should fix the handler

--- cardboardmodules/CardboardHandler.py
+++ cardboardmodules/CardboardHandler.py
@@ -22,7 +22,7 @@ class CardboardHandler:
 
         timestamp = int(time.time())
         sender = msg["mucnick"]
-        userjid = cmd.get_user_jid(connection, sender)
+        userjid = self.cmd.get_user_jid(connection, sender)
         message = msg["body"]
         
         fullmessage = sender + ": " + message
@@ -44,15 +44,15 @@ class CardboardHandler:
         if "!kick" in message.lower():
             log.debug("Kick command detected")
             to_kick = message.split("!kick ")[-1]
-            cmd.kick_user(connection, to_kick, sender, msg["from"].bare)
+            self.cmd.kick_user(connection, to_kick, sender, msg["from"].bare)
             return
 
         if "!identify" in message.lower():
             log.debug("Identify command detected")
             to_identify = message.split("!identify ")[-1]
-            affiliation = cmd.get_user_affiliation(connection, to_identify)
-            role = cmd.get_user_role(connection, to_identify)
-            userjid = cmd.get_user_jid(connection, to_identify)
+            affiliation = self.cmd.get_user_affiliation(connection, to_identify)
+            role = self.cmd.get_user_role(connection, to_identify)
+            userjid = self.cmd.get_user_jid(connection, to_identify)
             connection.send_message(mto=msg["from"].bare,
                                 mbody="%s was identified as %s, with role %s and affiliation %s" %(to_identify, userjid.bare, role, affiliation),
                                 mtype="groupchat")
@@ -66,35 +66,35 @@ class CardboardHandler:
             if message.lower().endswith("c/d") or message.lower().endswith("c/d?"):
                 log.debug("Confirm/deny detected")
                 connection.send_message(mto=msg["from"].bare,
-                                    mbody="%s: %s" %(sender, cmd.ceedee()),
+                                    mbody="%s: %s" %(sender, self.cmd.ceedee()),
                                     mtype="groupchat")
                 return
         
             # Someone does things to me!
             if message.lower().startswith("/me"):
                 log.debug("I am being touched by %s!" % sender)
-                connection.send_message(mto=msg["from"].bare, mbody=cmd.tuch(sender, message), mtype="groupchat")
+                connection.send_message(mto=msg["from"].bare, mbody=self.cmd.tuch(sender, message), mtype="groupchat")
                 return
         
             # Tumblr argueing
             if "argue" in message.lower():
                 log.debug("Someone wants me to argue!")
-                connection.send_message(mto=msg["from"].bare, mbody=cmd.argue(), mtype="groupchat")
+                connection.send_message(mto=msg["from"].bare, mbody=self.cmd.argue(), mtype="groupchat")
                 return
 
             # Tumblr rant
             if "rant" in message.lower():
                 log.debug("Someone wants me to rant!")
-                connection.send_message(mto=msg["from"].bare, mbody=cmd.rant(), mtype="groupchat")
+                connection.send_message(mto=msg["from"].bare, mbody=self.cmd.rant(), mtype="groupchat")
                 return
 
             # Delegate response to Alice
             log.debug("I don't know what to say, delegating to Alice")
-            connection.send_message(mto=msg["from"].bare, mbody=ai.alicemessage(sender, message), mtype="groupchat")
+            connection.send_message(mto=msg["from"].bare, mbody=self.ai.alicemessage(sender, message), mtype="groupchat")
             return
 
         # Handle links in messages
-        urls = links.handle_url(timestamp, userjid.bare, message)
+        urls = self.links.handle_url(timestamp, userjid.bare, message)
         if urls:
             connection.send_message(mto=msg["from"].bare, mbody=urls, mtype="groupchat")
         

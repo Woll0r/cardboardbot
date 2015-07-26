ac38862d52240eb1fb775ab89ac928f108832985
Wolfgang Vandevenne <woll0r@gmail.com>
2015-07-26 19:27:09 +0200

    Add a Markov chain brain option

---
+++ cardboardmodules/CardboardMarkov.py
@@ -0,0 +1,21 @@
+"""CardboardBot Markov chain brain"""
+
+import logging
+from pymarkovchain import MarkovChain
+
+log = logging.getLogger(__name__)
+
+
+class CardboardMarkov():
+    """CardboardBot Markov chain brain"""
+
+    def __init__(self, markovdb):
+        log.debug("CardboardMarkov init")
+        mc = MarkovChain(markovdb)
+    
+    def response(self, nick, body):
+        """Respond to a message"""
+
+        log.debug("CardboardMarkov response")
+
+        mc.generateString()
